e9ae32009735446ca6d9fc9f666595cab1c3d7f8
William Scalabre <william.scalabre@gmail.com>
2020-01-14 00:14:12 +0100

    feat: generate new board on each success

---
+++ src/constants/Cups.tsx
@@ -0,0 +1,5 @@
+export const cupsNumber: number = 3;
+
+export const easyCupsNumber: number = 3;
+export const mediumCupsNumber: number = 6;
+export const hardCupsNumber: number = 9;
--- src/containers/Board.tsx
+++ src/containers/Board.tsx
@@ -1,26 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Cup from "../components/Cup";
 import Ball from "../components/Ball";
 import Score from "../components/Score";
+import { getRandomInt } from "../helpers";
+import { cupsNumber } from "../constants/Cups";
 
 const Board: React.FC = () => {
   const [score, setScore] = useState(0);
+  const [board, setBoard] = useState([] as JSX.Element[]);
 
+  const ballPosition = getRandomInt(cupsNumber);
+
+  useEffect(() => {
+    const checkBallPosition = (position: number) => {
+      if (position === ballPosition) {
+        setScore(score + 1);
+        setBoard([]);
+      }
+    };
+    const Board: JSX.Element[] = [];
+    for (let index = 0; index < cupsNumber; index++) {
+      Board.push(
+        <div
+          className="Inline mr-2 Container"
+          onClick={() => checkBallPosition(index)}
+        >
+          <Cup />
+          {ballPosition === index && <Ball />}
+        </div>
+      );
+    }
+    setBoard(Board);
+  }, [score, ballPosition]);
+
+  console.log(board);
   return (
     <>
       <Header />
       <Score score={score} />
-      <div className="Inline mr-2 Container">
-        <Cup />
-        <Ball />
-      </div>
-      <div className="Inline mr-2">
-        <Cup />
-      </div>
-      <div className="Inline mr-2">
-        <Cup />
-      </div>
+      {board.length ? board : null}
     </>
   );
 };
